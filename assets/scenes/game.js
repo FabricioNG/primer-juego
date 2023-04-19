514e9aa27c7445909f413e849ef17fbb8706a952
FabricioNG <fabricio.navarro.gatti@gmail.com>
2023-04-18 21:58:19 -0300

    formas geometricas creadas

--- assets/scenes/game.js
+++ assets/scenes/game.js
@@ -8,17 +8,25 @@ export default class Game extends Phaser.Scene {
   preload(){
     this.load.image("sky", "./assets/images/sky.png");
     this.load.image("ground", "./assets/images/platform.png");    
-    this.load.image("ninja", "./assets/images/ninja.png");    
+    this.load.image("diamond", "./assets/images/diamond.png");    
+    this.load.image("triangle", "./assets/images/triangle.png");  
+    this.load.image("square", "./assets/images/square.png");  
+    this.load.image("ninja", "./assets/images/ninja.png");  
   }
 
   create() {
-    this.add.image(400, 300, "sky").setScale(0.555);
+    this.add.image(400, 300, "sky").setScale(0.555);//agrega el cielo o background
 
     let platforms = this.physics.add.staticGroup();
-    platforms.create(400, 568, "ground").setScale(2).refreshBody();
+    platforms.create(400, 568, "ground").setScale(2).refreshBody();//para actualizar ambos colider y se adaoten a la nueva pantalla
 
-    this.player = this.physics.add.sprite(100, 450, "ninja");
-    this.player.setCollideWorldBounds(true);
+    this.player = this.physics.add.sprite(100, 450, "ninja");//muestra el sprite del ninja
+    this.player.setCollideWorldBounds(true);//agegar colisiones al sprite
+
+    this.shapesGroup = this.physics.add.group();
+    this.shapesGroup.create(100, 0, "diamond");
+    this.shapesGroup.create(200, 0, "triangle");
+    this.shapesGroup.create(300, 0, "square");
   }
  
   update() {}
