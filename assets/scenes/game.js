c061c4874d13821048a16f0e2fe3d047039cf048
FabricioNG <fabricio.navarro.gatti@gmail.com>
2023-04-18 21:41:44 -0300

    agregue suelo

--- assets/scenes/game.js
+++ assets/scenes/game.js
@@ -7,10 +7,14 @@ export default class Game extends Phaser.Scene {
 
   preload(){
     this.load.image("sky", "./assets/images/sky.png");
+    this.load.image("ground", "./assets/images/platform.png");    
   }
 
   create() {
     this.add.image(400, 300, "sky").setScale(0.555);
+
+    let platforms = this.physics.add.staticGroup();
+    platforms.create(400, 568, "ground").setScale(2).refreshBody();
   }
  
   update() {}
