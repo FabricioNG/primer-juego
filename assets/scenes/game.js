9dffd83a84f61dbcf30890439d93ad122fd59e30
FabricioNG <fabricio.navarro.gatti@gmail.com>
2023-04-18 21:46:45 -0300

    colisiones

--- assets/scenes/game.js
+++ assets/scenes/game.js
@@ -8,6 +8,7 @@ export default class Game extends Phaser.Scene {
   preload(){
     this.load.image("sky", "./assets/images/sky.png");
     this.load.image("ground", "./assets/images/platform.png");    
+    this.load.image("ninja", "./assets/images/ninja.png");    
   }
 
   create() {
@@ -15,6 +16,9 @@ export default class Game extends Phaser.Scene {
 
     let platforms = this.physics.add.staticGroup();
     platforms.create(400, 568, "ground").setScale(2).refreshBody();
+
+    this.player = this.physics.add.sprite(100, 450, "ninja");
+    this.player.setCollideWorldBounds(true);
   }
  
   update() {}
